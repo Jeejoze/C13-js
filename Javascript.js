26dfdbc076557f028615a5a29b860915ae8bd44a
Jeejo <pannatat.jeejo@gmail.com>
2023-06-14 17:12:03 +0700

    first commit

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+let textCount = document.querySelector("#textCount");
+let textshow = document.querySelector("#textran");
+let text
+let countnum = 0;
+let ranmax = 100;
+let ranmin = 0;
+let correct = false;
+textCount.textContent += countnum;
+do{
+   
+}while(!correct);
