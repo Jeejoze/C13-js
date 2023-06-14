593c7d0eaeb77111513d114abd620a4fe93ebe21
Jeejo <pannatat.jeejo@gmail.com>
2023-06-14 18:19:26 +0700

    gg

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,44 @@
 let textCount = document.querySelector("#textCount");
-let textshow = document.querySelector("#textran");
-let text
+let textshow = document.querySelector("#textRan");
+let btn1 = document.querySelector("#btn1");
+let btnhigh = document.querySelector("#btnhigh");
+let btnlow = document.querySelector("#btnlow");
+let btncorrect = document.querySelector("#btnc");
 let countnum = 0;
 let ranmax = 100;
-let ranmin = 0;
 let correct = false;
-textCount.textContent += countnum;
-do{
-   
-}while(!correct);
+btnhigh.disabled = true;
+btncorrect.disabled = true;
+btnlow.disabled = true;
+textCount.textContent = `Count : ${countnum}`;
+function IsStart(a) {
+    if (btn1.textContent == "Start?") {
+        btn1.textContent = "You are playing";
+        btn1.disabled = true;
+        textCount.textContent = `Count : ${countnum}`;
+        let ran = Math.floor(Math.random() * ranmax);
+        textshow.textContent = `RandomNumber is : ${ran}`;
+        btnhigh.disabled = false;
+        btncorrect.disabled = false;
+        btnlow.disabled = false;
+    }
+    switch (a) {
+        case 1:
+            break;
+        case 2:
+            btn1.disabled = false;
+            countnum = 0;
+            break;
+        case 3:
+            break;
+    }
+
+
+}
+function IsPlaying() {
+
+}
+btn1.addEventListener("click", IsStart(0));
+btnhigh.addEventListener("click",IsStart(1));
+btncorrect.addEventListener("click",IsStart(2));
+btnlow.addEventListener("click",IsStart(3));
